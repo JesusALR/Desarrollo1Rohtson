13587062c436cf7004256b1deb878365f69db7e6
Isaac Espinoza G <l18330428@hermosillo.tecnm.mx>
2021-11-13 00:50:22 -0700

    Hicimos cambios en el LoginApp, se agrego el logo, y tambien cambiamos lo que es la direccion con el que se ejecuta el software.

---
+++ Rohtson/LoginApp.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Rohtson
+{
+    partial class LoginApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.btnSalirProg = new Guna.UI2.WinForms.Guna2Button();
+            this.bntEntrar = new Guna.UI2.WinForms.Guna2Button();
+            this.linkLogin = new System.Windows.Forms.LinkLabel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Image = global::Rohtson.Properties.Resources.LogoRohtson;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.ShadowDecoration.Parent = this.guna2PictureBox1;
+            this.guna2PictureBox1.Size = new System.Drawing.Size(164, 81);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox1.TabIndex = 11;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // btnSalirProg
+            // 
+            this.btnSalirProg.BackColor = System.Drawing.Color.Transparent;
+            this.btnSalirProg.CheckedState.Parent = this.btnSalirProg;
+            this.btnSalirProg.CustomImages.Parent = this.btnSalirProg;
+            this.btnSalirProg.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnSalirProg.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalirProg.ForeColor = System.Drawing.Color.White;
+            this.btnSalirProg.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnSalirProg.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnSalirProg.HoverState.Parent = this.btnSalirProg;
+            this.btnSalirProg.Location = new System.Drawing.Point(515, 0);
+            this.btnSalirProg.Name = "btnSalirProg";
+            this.btnSalirProg.ShadowDecoration.Parent = this.btnSalirProg;
+            this.btnSalirProg.Size = new System.Drawing.Size(50, 45);
+            this.btnSalirProg.TabIndex = 26;
+            this.btnSalirProg.Text = "X";
+            this.btnSalirProg.Click += new System.EventHandler(this.btnSalirProg_Click);
+            // 
+            // bntEntrar
+            // 
+            this.bntEntrar.CheckedState.Parent = this.bntEntrar;
+            this.bntEntrar.CustomImages.Parent = this.bntEntrar;
+            this.bntEntrar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.bntEntrar.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.bntEntrar.ForeColor = System.Drawing.Color.Black;
+            this.bntEntrar.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.bntEntrar.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.bntEntrar.HoverState.Parent = this.bntEntrar;
+            this.bntEntrar.Location = new System.Drawing.Point(200, 330);
+            this.bntEntrar.Name = "bntEntrar";
+            this.bntEntrar.ShadowDecoration.Parent = this.bntEntrar;
+            this.bntEntrar.Size = new System.Drawing.Size(180, 45);
+            this.bntEntrar.TabIndex = 25;
+            this.bntEntrar.Text = "Entrar";
+            // 
+            // linkLogin
+            // 
+            this.linkLogin.AutoSize = true;
+            this.linkLogin.BackColor = System.Drawing.Color.Transparent;
+            this.linkLogin.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.linkLogin.Location = new System.Drawing.Point(235, 307);
+            this.linkLogin.Name = "linkLogin";
+            this.linkLogin.Size = new System.Drawing.Size(112, 13);
+            this.linkLogin.TabIndex = 24;
+            this.linkLogin.TabStop = true;
+            this.linkLogin.Text = "Restaurar contraseña:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 253);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(243, 20);
+            this.textBox2.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(230, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 23);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Contraseña:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 181);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(249, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 23);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Usuario:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Brush Script MT", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblTitulo.Location = new System.Drawing.Point(-6, 94);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(222, 33);
+            this.lblTitulo.TabIndex = 19;
+            this.lblTitulo.Text = "Bienvenido de vuelta!";
+            // 
+            // LoginApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(565, 395);
+            this.Controls.Add(this.btnSalirProg);
+            this.Controls.Add(this.bntEntrar);
+            this.Controls.Add(this.linkLogin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginApp";
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2Button btnSalirProg;
+        private Guna.UI2.WinForms.Guna2Button bntEntrar;
+        private System.Windows.Forms.LinkLabel linkLogin;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
--- Rohtson/Program.cs
+++ Rohtson/Program.cs
@@ -16,7 +16,7 @@ namespace Rohtson
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginApp());
         }
     }
 }
